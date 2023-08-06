7514a431b5c7951bbd36c3296e1d22083e205d34
digmen <digmen68@gmail.com>
2023-08-06 14:38:17 +0600

    lama 4

--- src/page/AdminPage.jsx
+++ src/page/AdminPage.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import axios from 'axios';
+import './css/main.css';
+import { useToast } from '@chakra-ui/react';
 
 function AdminPage() {
-    return (
-        <div>
-            
-        </div>
-    );
+  const fileInputRef = React.useRef();
+  const toast = useToast();
+
+  const handleFileUpload = async () => {
+    const files = fileInputRef.current.files;
+
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append(`file${i}`, files[i]);
+    }
+
+    const firebaseURL = 'YOUR_FIREBASE_URL';
+
+    try {
+      await axios.post(firebaseURL, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      alert('Данные загружены');
+    } catch (error) {
+      alert('Ошибка');
+    }
+  };
+
+  return (
+    <div className="admin">
+      <h1>Upload File</h1>
+      <form>
+        <input type="file" multiple ref={fileInputRef} />
+        <button className="btn" type="button" onClick={handleFileUpload}>
+          Upload
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default AdminPage;
